65691008418f4aa139b78aeae6f218e82b5e260e
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2022-07-08 17:42:03 -0600

    Time: 1703 ms (20.18%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -15,3 +15,14 @@ and p.product_id not in (
     where sale_date not between '2019-01-01' and '2019-03-31'
 );
 
+-- simpliest solution
+
+/*
+SELECT s.product_id, product_name
+FROM Sales s
+LEFT JOIN Product p
+ON s.product_id = p.product_id
+GROUP BY s.product_id
+HAVING MIN(sale_date) >= CAST('2019-01-01' AS DATE) AND
+       MAX(sale_date) <= CAST('2019-03-31' AS DATE)
+*/
