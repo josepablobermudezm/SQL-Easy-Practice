4804f4add26313ca6949f1232ee8ccd6613bf35b
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2022-07-08 15:15:38 -0600

    Time: 427 ms (50.17%), Space: 0B (100.00%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,17 +1,12 @@
 # Write your MySQL query statement below
 
-with a as (
 select 
-    count(actor_id) as counta, 
-    count(director_id) as countd, 
-    actor_id, 
+    actor_id,
+    director_id 
+from actordirector 
+group by 
+    actor_id,
     director_id
-from ActorDirector 
-group by director_id, actor_id
-having count(actor_id) >= 3 and count(director_id) >= 3 
-)
-
-select actor_id, director_id
-from a
+having count(*)>=3;    
     
 
