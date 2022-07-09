b7004238ada08ec35d3e23ac7f74a15c78284ab6
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2022-07-08 20:36:53 -0600

    Create  1873-calculate-special-bonus.sql

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+SELECT employee_id 
+FROM employees
+WHERE employee_id NOT IN (SELECT employee_id FROM salaries)
+UNION
+SELECT employee_id 
+FROM salaries
+WHERE employee_id NOT IN (SELECT employee_id FROM employees)
+ORDER BY employee_id;
